2f09186db9e22fa23595d39a91283bf43cbf9052
saulius <sjsaulius@gmail.com>
2025-01-17 15:20:58 +0200

    create database connection

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require("express");
-const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/camp");
+const Campground = require("./models/camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("start");
+});
+app.get("/makecamp", async (req, res) => {
+  const camp = new Campground({ title: "Make camp great again", description: "cool camp!!" });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
