3c1aecdc49e9c24e86d0182826d73c2bab077060
saulius <sjsaulius@gmail.com>
2025-02-04 14:55:49 +0200

    add img css

--- seeds/index.js
+++ seeds/index.js
@@ -16,9 +16,13 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city},${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique eos reiciendis fugit ipsa expedita alias ab quod, pariatur facere, quia ut error distinctio nobis adipisci incidunt itaque sed dicta ratione?",
+      price,
     });
     await camp.save();
   }
