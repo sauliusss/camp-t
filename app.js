4c5a6197be2037c47586984816b589e809769c19
saulius <sjsaulius@gmail.com>
2025-03-05 13:46:47 +0200

    register logic

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const ejsMate = require("ejs-mate");
 const session = require("express-session");
 const flash = require("connect-flash");
 const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const campgrounds = require("./routes/campground.js");
-const reviews = require("./routes/reviews.js");
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-const { title } = require("process");
-const { error } = require("console");
+const userRoutes = require("./routes/users.js");
+const campgroundRoutes = require("./routes/campground.js");
+const reviewRoutes = require("./routes/reviews.js");
+
+// const { title } = require("process");
+// const { error } = require("console");
 
 mongoose.connect("mongodb://localhost:27017/camp");
 
@@ -43,14 +50,19 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
