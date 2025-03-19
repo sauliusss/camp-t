130e38144e5f17b9d7f826df51be3b10c138d0cc
saulius <sjsaulius@gmail.com>
2025-03-19 14:03:30 +0200

    made upload pictures to cloudinary function

--- routes/campground.js
+++ routes/campground.js
@@ -4,7 +4,8 @@ const campgrounds = require("../controllers/campgrounds.js");
 const catchAsync = require("../utils/catchAsync");
 const { isLoggedIn, isAuthor, validateCampground } = require("../middleware.js");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { storage } = require("../cloudinary");
+const upload = multer({ storage });
 
 const Campground = require("../models/campground");
 const { path } = require("express/lib/application.js");
